16bf0df951bb03b7b21f556a5944d683b6d9a3b0
Nox-Box <56707979+Nox-Box@users.noreply.github.com>
2020-11-02 23:31:46 +0700

    Лабораторная работа №4 2-11-2020

---
+++ Лабораторная работа №4 2-11-2020/lab 4.1.1/lab 4.1.1/lab 4.1.1.cpp
@@ -0,0 +1,54 @@
+﻿// lab 4.1.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+//Функция проверки двух стеков на "палиндромность"
+void check(stack<char>& firstStack, stack<char>& secondStack)
+{
+    //Пока оба стека не пусты
+    while (!firstStack.empty() && !secondStack.empty())
+    {
+        //Если головы стеков не равны
+        if (toupper(firstStack.top()) != toupper(secondStack.top()))
+        {
+            cout << "Строка не является палиндромом" << endl;
+            return;
+        }
+
+        //Удаляем текущие первые элементы в стеках
+        firstStack.pop();
+        secondStack.pop();
+    }
+
+    cout << "Строка является палиндромом" << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    //Объявляем первый массив
+    string str;
+
+    //Читаем в него данные с консоли
+    cin >> str;
+
+    //Объявляем два стека
+    stack<char> firstStack;
+    stack<char> secondStack;
+
+    //Заносим в первый - первую половину строки
+    for (auto i = str.begin(); i != str.begin() + (str.end() - str.begin()) / 2; ++i)
+        firstStack.push(*i);
+
+    //Во второй - вторую, но с конца
+    for (auto i = str.rbegin(); i != str.rbegin() + (str.rend() - str.rbegin()) / 2; ++i)
+        secondStack.push(*i);
+
+    //Проверяем оба стека (ОСТОРОЖНО! Эта функция портит стеки и после её вызова они окажутся пустыми)
+    check(firstStack, secondStack);
+
+}
